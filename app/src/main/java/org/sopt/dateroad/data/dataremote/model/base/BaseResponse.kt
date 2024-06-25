5f284030073d69acb0e32d7829bc175746cd367f
jihyunniiii <jita1125@naver.com>
2024-06-25 15:17:33 +0900

    [feat] #1 add baseResponse

---
+++ app/src/main/java/org/sopt/dateroad/data/dataremote/model/base/BaseResponse.kt
@@ -0,0 +1,14 @@
+package org.sopt.dateroad.data.dataremote.model.base
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class BaseResponse<T>(
+    @SerialName("code")
+    val code: Int,
+    @SerialName("message")
+    val message: String,
+    @SerialName("data")
+    val data: T
+)
