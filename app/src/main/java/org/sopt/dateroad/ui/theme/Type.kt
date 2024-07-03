01abe19326bccccf652c540c72efad8a7bde94e4
t1nm1ksun <minsuk07672@naver.com>
2024-07-03 15:42:30 +0900

    [chore] 변수명 카멜케이스 적용, 주석 요약

--- app/src/main/java/org/sopt/dateroad/ui/theme/Type.kt
+++ app/src/main/java/org/sopt/dateroad/ui/theme/Type.kt
@@ -35,112 +35,112 @@ val Typography = Typography(
     */
 )
 
-// title font setting
-val title_extra_24 = TextStyle(
+// Title
+val titleExtra24 = TextStyle(
     fontFamily = FontFamily(Font(R.font.suit_extrabold)),
     fontSize = 24.sp,
     lineHeight = 24.sp * 1.3
 )
 
-val title_extra_20 = TextStyle(
+val titleExtra20 = TextStyle(
     fontFamily = FontFamily(Font(R.font.suit_extrabold)),
     fontSize = 20.sp,
     lineHeight = 20.sp * 1.4
 )
 
-val title_bold_20 = TextStyle(
+val titleBold20 = TextStyle(
     fontFamily = FontFamily(Font(R.font.suit_bold)),
     fontSize = 20.sp,
     lineHeight = 20.sp * 1.4
 )
 
-val title_bold_18 = TextStyle(
+val titleBold18 = TextStyle(
     fontFamily = FontFamily(Font(R.font.suit_bold)),
     fontSize = 18.sp,
     lineHeight = 18.sp * 1.4
 )
 
-val title_med_18 = TextStyle(
+val titleMed18 = TextStyle(
     fontFamily = FontFamily(Font(R.font.suit_medium)),
     fontSize = 18.sp,
     lineHeight = 18.sp * 1.4
 )
 
-// body font setting
-val body_bold_17 = TextStyle(
+// Body
+val bodyBold17 = TextStyle(
     fontFamily = FontFamily(Font(R.font.suit_bold)),
     fontSize = 17.sp,
     lineHeight = 17.sp * 1.4
 )
 
-val body_semi_17 = TextStyle(
+val bodySemi17 = TextStyle(
     fontFamily = FontFamily(Font(R.font.suit_semibold)),
     fontSize = 17.sp,
     lineHeight = 17.sp * 1.4
 )
 
-val body_med_17 = TextStyle(
+val bodyMed17 = TextStyle(
     fontFamily = FontFamily(Font(R.font.suit_medium)),
     fontSize = 17.sp,
     lineHeight = 17.sp * 1.4
 )
 
-val body_bold_15 = TextStyle(
+val bodyBold15 = TextStyle(
     fontFamily = FontFamily(Font(R.font.suit_bold)),
     fontSize = 15.sp,
     lineHeight = 15.sp * 1.4
 )
 
-val body_bold_15_course = TextStyle(
+val bodyBold15Course = TextStyle(
     fontFamily = FontFamily(Font(R.font.suit_bold)),
     fontSize = 15.sp,
     lineHeight = 15.sp * 1.3
 )
 
-val body_semi_15 = TextStyle(
+val bodySemi15 = TextStyle(
     fontFamily = FontFamily(Font(R.font.suit_semibold)),
     fontSize = 15.sp,
     lineHeight = 15.sp * 1.4
 )
 
-val body_med_15 = TextStyle(
+val bodyMed15 = TextStyle(
     fontFamily = FontFamily(Font(R.font.suit_medium)),
     fontSize = 15.sp,
     lineHeight = 15.sp * 1.4
 )
 
-val body_bold_13 = TextStyle(
+val bodyBold13 = TextStyle(
     fontFamily = FontFamily(Font(R.font.suit_bold)),
     fontSize = 13.sp,
     lineHeight = 13.sp * 1.4
 )
 
-val body_semi_13 = TextStyle(
+val bodySemi13 = TextStyle(
     fontFamily = FontFamily(Font(R.font.suit_semibold)),
     fontSize = 13.sp,
     lineHeight = 13.sp * 1.4
 )
 
-val body_med_13 = TextStyle(
+val bodyMed13 = TextStyle(
     fontFamily = FontFamily(Font(R.font.suit_medium)),
     fontSize = 13.sp,
     lineHeight = 13.sp * 1.4
 )
 
-val body_med_13_context = TextStyle(
+val bodyMed13Context = TextStyle(
     fontFamily = FontFamily(Font(R.font.suit_medium)),
     fontSize = 13.sp,
     lineHeight = 13.sp * 1.5
 )
 
-// caption font setting
-val cap_bold_11 = TextStyle(
+// Caption
+val capBold11 = TextStyle(
     fontFamily = FontFamily(Font(R.font.suit_bold)),
     fontSize = 11.sp,
     lineHeight = 11.sp * 1.4
 )
 
-val cap_reg_11 = TextStyle(
+val capReg11 = TextStyle(
     fontFamily = FontFamily(Font(R.font.suit_regular)),
     fontSize = 11.sp,
     lineHeight = 11.sp * 1.4
