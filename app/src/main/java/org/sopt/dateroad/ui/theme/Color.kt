f61280813fabd32d59c6bc50e9c3f3676d5c4467
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-07-04 01:48:52 +0900

    [init] #5 컬러 초기세팅

--- app/src/main/java/org/sopt/dateroad/ui/theme/Color.kt
+++ app/src/main/java/org/sopt/dateroad/ui/theme/Color.kt
@@ -2,10 +2,35 @@ package org.sopt.dateroad.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val Purple40 = Color(0xFF6650a4)
 val Purple80 = Color(0xFFD0BCFF)
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val DeepPurple = Color(0xFF4A3CEF)
+val MediumPurlple = Color(0xFF6E61FF)
+val LightPurple = Color(0xFFA8A0FF)
+val Lilac = Color(0xFFD5B5EE)
+
+
+val PurpleGrey40 = Color(0xFF625b71)
 val PurpleGrey80 = Color(0xFFCCC2DC)
+
+val Pink = Color(0xFFE3A3F9)
+val Pink40 = Color(0xFF7D5260)
 val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Lime = Color(0xFFDFF37C)
+val LightLime = Color(0xFFE5F3AE)
+
+val Teal200 = Color(0xFF03DAC5)
+val Teal700 = Color(0xFF018786)
+
+val Black = Color(0xFF090909)
+val Gray600 = Color(0xFF080909)
+val Gray500 = Color(0xFF53525B)
+val Gray400 = Color(0xFF7B7C87)
+val Gray300 = Color(0xFFAEAFBC)
+val Gray200 = Color(0xFFD5D5DE)
+val Gray100 = Color(0xFFEBEBF3)
+val White = Color(0xFFFFFFFF)
