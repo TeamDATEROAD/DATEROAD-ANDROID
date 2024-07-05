efe9624c0624078ad1ae34a8abe0d91833f2eff3
jihyunniiii <jita1125@naver.com>
2024-07-05 10:23:52 +0900

    [feat] #10 MoneyTagType

---
+++ app/src/main/java/org/sopt/dateroad/presentation/type/MoneyTagType.kt
@@ -0,0 +1,21 @@
+package org.sopt.dateroad.presentation.type
+
+import androidx.annotation.StringRes
+import org.sopt.dateroad.R
+
+enum class MoneyTagType(
+    @StringRes titleRes: Int
+) {
+    EXCESS_100000(
+        titleRes = R.string.money_tag_excess_100000
+    ),
+    LESS_THAN_100000(
+        titleRes = R.string.money_tag_less_than_100000
+    ),
+    LESS_THAN_50000(
+        titleRes = R.string.money_tag_less_than_50000
+    ),
+    LESS_THAN_30000(
+        titleRes = R.string.money_tag_less_than_30000
+    )
+}
