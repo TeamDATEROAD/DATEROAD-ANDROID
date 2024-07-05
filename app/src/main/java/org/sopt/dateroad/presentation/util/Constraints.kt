12df72d05834f0e02aee4689dc579b4ec6931e4b
jihyunniiii <jita1125@naver.com>
2024-07-05 11:29:56 +0900

    [feat] #10 PointHistoryTabType

--- app/src/main/java/org/sopt/dateroad/presentation/util/Constraints.kt
+++ app/src/main/java/org/sopt/dateroad/presentation/util/Constraints.kt
@@ -3,3 +3,8 @@ package org.sopt.dateroad.presentation.util
 object BottomSheet
 
 object Dialog
+
+object PointHistoryTab{
+    const val EARN_HISTORY_POSITION = 0
+    const val USAGE_HISTORY_POSITION = 1
+}
