152b07748e6c6642601c18c403fbc240803bff37
jihyunniiii <jita1125@naver.com>
2024-07-05 11:10:39 +0900

    [feat] #10 IncheonAreaType

---
+++ app/src/main/java/org/sopt/dateroad/presentation/type/IncheonAreaType.kt
@@ -0,0 +1,12 @@
+package org.sopt.dateroad.presentation.type
+
+import androidx.annotation.StringRes
+import org.sopt.dateroad.R
+
+enum class IncheonAreaType(
+    @StringRes val nameRes: Int
+) {
+    WHOLE(
+        nameRes = R.string.incheon_area_whole
+    )
+}
