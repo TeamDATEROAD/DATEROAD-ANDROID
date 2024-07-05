18b7708628f7e6bf24a6b597e6caff60f020a0ab
jihyunniiii <jita1125@naver.com>
2024-07-06 03:37:35 +0900

    [feat] #17 예시 파일 추가

--- app/src/main/java/org/sopt/dateroad/presentation/type/PointBoxType.kt
+++ app/src/main/java/org/sopt/dateroad/presentation/type/PointBoxType.kt
@@ -2,9 +2,12 @@ package org.sopt.dateroad.presentation.type
 
 import androidx.compose.ui.graphics.Color
 import org.sopt.dateroad.ui.theme.Black
+import org.sopt.dateroad.ui.theme.DateRoadColors
+import org.sopt.dateroad.ui.theme.DateRoadTheme
 import org.sopt.dateroad.ui.theme.DeepPurple
 import org.sopt.dateroad.ui.theme.Gray400
 import org.sopt.dateroad.ui.theme.White
+import org.sopt.dateroad.ui.theme.defaultDateRoadColors
 
 enum class PointBoxType(
     backgroundColor: Color,
@@ -12,13 +15,13 @@ enum class PointBoxType(
     pointTextColor: Color
 ) {
     POINT_HISTORY(
-        backgroundColor = DeepPurple,
-        titleTextColor = White,
-        pointTextColor = White
+        backgroundColor = defaultDateRoadColors.deepPurple,
+        titleTextColor = defaultDateRoadColors.white,
+        pointTextColor = defaultDateRoadColors.white
     ),
     MY_PAGE(
-        backgroundColor = White,
-        titleTextColor = Gray400,
-        pointTextColor = Black
+        backgroundColor = defaultDateRoadColors.white,
+        titleTextColor = defaultDateRoadColors.gray400,
+        pointTextColor = defaultDateRoadColors.black
     )
 }
