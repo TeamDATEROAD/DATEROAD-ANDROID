11d95b6f7b1c53af18ad0f1dae2864a8dffb8048
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-07-05 23:41:31 +0900

    [chore] #5 color 관련파일 수정

--- app/src/main/java/org/sopt/dateroad/ui/theme/Theme.kt
+++ app/src/main/java/org/sopt/dateroad/ui/theme/Theme.kt
@@ -11,15 +11,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Pink,
+    secondary = MediumPurple,
+    tertiary = Lilac
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Pink,
+    secondary = MediumPurple,
+    tertiary = Lilac
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
