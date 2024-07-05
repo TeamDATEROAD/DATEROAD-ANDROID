84b989c68e4bb97bd0208c22a5726c7fd9fb65f4
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-07-05 23:36:49 +0900

    [chore] #5 color 초기세팅 수정

--- app/src/main/java/org/sopt/dateroad/ui/theme/Color.kt
+++ app/src/main/java/org/sopt/dateroad/ui/theme/Color.kt
@@ -3,6 +3,10 @@ package org.sopt.dateroad.ui.theme
 import androidx.compose.ui.graphics.Color
 
 // Main
+val Purple40 = Color(0xFF6650a4)
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey40 = Color(0xFF625b71)
+val PurpleGrey80 = Color(0xFFCCC2DC)
 val DeepPurple = Color(0xFF4A3CEF)
 val MediumPurple = Color(0xFF6E61FF)
 val LightPurple = Color(0xFFA8A0FF)
@@ -11,6 +15,8 @@ val LightPink = Color(0xFFEFD6F8)
 val Pink = Color(0xFFE3A3F9)
 val Lime = Color(0xFFDFF37C)
 val LightLime = Color(0xFFE5F3AE)
+val Pink40 = Color(0xFF7D5260)
+val Pink80 = Color(0xFFEFB8C8)
 
 // GrayScale
 val Black = Color(0xFF090909)
@@ -21,6 +27,7 @@ val Gray300 = Color(0xFFAEAFBC)
 val Gray200 = Color(0xFFD5D5DE)
 val Gray100 = Color(0xFFEBEBF3)
 val White = Color(0xFFFFFFFF)
+
 
 // Notif
 val AlertRed = Color(0xFFFF0000)
