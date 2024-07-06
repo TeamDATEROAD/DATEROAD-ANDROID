732763c3371699728f85336a2662c1e2da0f40fc
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-07-06 17:06:23 +0900

    [feat] #13 DateRoadFieldTag

---
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/component/tag/DateRoadFieldTag.kt
@@ -0,0 +1,52 @@
+package org.sopt.dateroad.presentation.ui.component.tag
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.sopt.dateroad.ui.theme.Black
+import org.sopt.dateroad.ui.theme.DATEROADTheme
+import org.sopt.dateroad.ui.theme.DateRoadTheme
+import org.sopt.dateroad.ui.theme.LightPink
+
+@Composable
+fun DateRoadFieldTag(
+    modifier: Modifier = Modifier,
+    text: String,
+    backgroundColor: Color = LightPink,
+    contentColor: Color = Black,
+) {
+    Row(
+        modifier = modifier
+            .clip(RoundedCornerShape(20.dp))
+            .background(backgroundColor)
+            .padding(horizontal = 10.dp, vertical = 4.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = text,
+            style = DateRoadTheme.typography.bodyMed13,
+            color = contentColor,
+            maxLines = 1,
+            modifier = Modifier.align(Alignment.CenterVertically)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun DDateRoadFieldTagPreview() {
+    DATEROADTheme {
+        DateRoadFieldTag(
+            text = "🎨 전시-팝업",
+        )
+    }
+}
