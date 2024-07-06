780cf0576bcc66037584c2383ce0c175d3e1e249
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-07-06 17:14:38 +0900

    [feat] #13 DateRoadPlaceCardNumberTag

---
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/component/tag/DateRoadPlaceCardNumberTag.kt
@@ -0,0 +1,51 @@
+package org.sopt.dateroad.presentation.ui.component.tag
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.sopt.dateroad.ui.theme.DATEROADTheme
+import org.sopt.dateroad.ui.theme.DateRoadTheme
+import org.sopt.dateroad.ui.theme.DeepPurple
+import org.sopt.dateroad.ui.theme.White
+
+@Composable
+fun DateRoadPlaceCardNumberTag(
+    modifier: Modifier = Modifier,
+    text: String,
+    backgroundColor: Color = DeepPurple,
+    contentColor: Color = White,
+) {
+    Row(
+        modifier = modifier
+            .clip(RoundedCornerShape(50.dp))
+            .background(backgroundColor)
+            .padding(horizontal = 9.dp, vertical = 4.dp),
+    ) {
+        Text(
+            text = text,
+            style = DateRoadTheme.typography.bodyBold13,
+            color = contentColor,
+            maxLines = 1,
+            modifier = Modifier.align(Alignment.CenterVertically)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun DateRoadPlaceCardNumberTagPreview() {
+    DATEROADTheme {
+        DateRoadPlaceCardNumberTag(
+            text = "1",
+        )
+    }
+}
