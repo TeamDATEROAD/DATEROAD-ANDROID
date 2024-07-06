15ffa4c02b3339ea8e26f914c4d5d9325359a414
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-07-06 21:35:59 +0900

    [feat] #9 DateRoadNextButton

---
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/component/button/DateRoadNextButton.kt
@@ -0,0 +1,50 @@
+package org.sopt.dateroad.presentation.ui.component.button
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.sopt.dateroad.ui.theme.DATEROADTheme
+import org.sopt.dateroad.ui.theme.DateRoadTheme
+import org.sopt.dateroad.ui.theme.DeepPurple
+import org.sopt.dateroad.ui.theme.White
+
+@Composable
+fun DateRoadNextButton(
+    modifier: Modifier = Modifier,
+    backgroundColor: Color = DeepPurple,
+    contentColor: Color = White
+) {
+    Row(
+        modifier = modifier
+            .clip(RoundedCornerShape(10.dp))
+            .background(backgroundColor)
+            .padding(vertical = 10.dp, horizontal = 20.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "데이트 코스 올리고 50P 받기",
+            style = DateRoadTheme.typography.bodyBold15,
+            color = contentColor,
+            modifier = Modifier.align(Alignment.CenterVertically)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun DateRoadNextButtonPreview() {
+    DATEROADTheme {
+        DateRoadNextButton()
+    }
+}
