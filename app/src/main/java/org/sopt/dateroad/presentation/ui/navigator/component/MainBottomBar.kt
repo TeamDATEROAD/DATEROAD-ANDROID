d1cffc383ecbb15d091c38fc5357075da011e710
t1nm1ksun <minsuk07672@naver.com>
2024-07-06 15:39:34 +0900

    [chore] #14 selected 배경색, border 적용

--- app/src/main/java/org/sopt/dateroad/presentation/ui/navigator/component/MainBottomBar.kt
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/navigator/component/MainBottomBar.kt
@@ -2,21 +2,27 @@ package org.sopt.dateroad.presentation.ui.navigator.component
 
 import android.content.Context
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.border
+import androidx.compose.foundation.background
+import androidx.compose.material3.*
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.sopt.dateroad.presentation.type.MainNavigationBarItemType
 import org.sopt.dateroad.ui.theme.Black
 import org.sopt.dateroad.ui.theme.DATEROADTheme
+import org.sopt.dateroad.ui.theme.DateRoadTheme
 import org.sopt.dateroad.ui.theme.Gray200
 import org.sopt.dateroad.ui.theme.Gray300
-import org.sopt.dateroad.ui.theme.capReg11
+import org.sopt.dateroad.ui.theme.White
 
 @Composable
 fun MainBottomBar(
@@ -28,7 +34,12 @@ fun MainBottomBar(
     onNavigationBarItemSelected: (MainNavigationBarItemType) -> Unit
 ) {
     AnimatedVisibility(visible = isVisible) {
-        NavigationBar {
+        NavigationBar(
+            modifier = modifier
+                .background(White)
+                .border(1.dp, Color(0xFFF1F1F5)),
+            containerColor = White
+        ) {
             navigationBarItems.forEachIndexed { _, mainNavigationBarItemType ->
                 val isSelected = currentNavigationBarItem == mainNavigationBarItemType
 
@@ -45,10 +56,18 @@ fun MainBottomBar(
                     label = {
                         Text(
                             text = context.getString(mainNavigationBarItemType.label),
-                            style = capReg11,
+                            style = DateRoadTheme.typography.capReg11,
                             color = if (isSelected) Black else Gray300
                         )
-                    }
+                    },
+                    colors = NavigationBarItemDefaults.colors(
+                        selectedIconColor = Black,
+                        unselectedIconColor = Gray200,
+                        selectedTextColor = Black,
+                        unselectedTextColor = Gray300,
+                        indicatorColor = White // 선택된 아이템의 배경색을 흰색으로 설정
+                    ),
+                    modifier = Modifier.background(White)
                 )
             }
         }
