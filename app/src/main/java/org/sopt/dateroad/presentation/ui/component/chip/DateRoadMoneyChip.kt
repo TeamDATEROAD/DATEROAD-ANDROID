569fe1efd6ff0b805323dfe2abbd2f6a1c43e980
t1nm1ksun <minsuk07672@naver.com>
2024-07-06 17:42:43 +0900

    [chore] #20 clickable 액션 제거

--- app/src/main/java/org/sopt/dateroad/presentation/ui/component/chip/DateRoadMoneyChip.kt
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/component/chip/DateRoadMoneyChip.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.sopt.dateroad.R
+import org.sopt.dateroad.presentation.util.modifier.noRippleClickable
 import org.sopt.dateroad.ui.theme.DateRoadTheme
 import org.sopt.dateroad.ui.theme.DeepPurple
 import org.sopt.dateroad.ui.theme.Gray100
@@ -37,7 +38,7 @@ fun DateRoadMoneyChip(
         modifier = Modifier
             .clip(RoundedCornerShape(20.dp))
             .background(color = if (selected) DeepPurple else Gray100)
-            .clickable {
+            .noRippleClickable  {
                 selected = !selected
                 onSelectedChange(selected)
             },
--- app/src/main/java/org/sopt/dateroad/presentation/ui/component/chip/DateRoadTagChip.kt
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/component/chip/DateRoadTagChip.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.sopt.dateroad.R
+import org.sopt.dateroad.presentation.util.modifier.noRippleClickable
 import org.sopt.dateroad.ui.theme.Black
 import org.sopt.dateroad.ui.theme.DateRoadTheme
 import org.sopt.dateroad.ui.theme.DeepPurple
@@ -39,7 +40,7 @@ fun DateRoadTagChip(
         modifier = Modifier
             .clip(RoundedCornerShape(20.dp))
             .background(color = if (selected) DeepPurple else Gray100)
-            .clickable {
+            .noRippleClickable  {
                 selected = !selected
                 onSelectedChange(selected)
             },
