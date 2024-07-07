24ffe245ae11aeec2e77a958a4cc12462d578482
t1nm1ksun <minsuk07672@naver.com>
2024-07-07 20:52:53 +0900

    [chore] #22 string 추출, color값 Theme에서 불러오기

--- app/src/main/java/org/sopt/dateroad/presentation/ui/component/pointbox/DateRoadMyPagePointBox.kt
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/component/pointbox/DateRoadMyPagePointBox.kt
@@ -6,9 +6,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
@@ -22,35 +22,38 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.sopt.dateroad.R
-import org.sopt.dateroad.ui.theme.Black
 import org.sopt.dateroad.ui.theme.DateRoadTheme
-import org.sopt.dateroad.ui.theme.Gray400
-import org.sopt.dateroad.ui.theme.White
 
 @Composable
 fun DateRoadMyPagePointBox(nickname: String, point: Int) {
     Box(
         modifier = Modifier
-            .size(width = 328.dp, height = 90.dp)
+            .height(90.dp)
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
             .clip(RoundedCornerShape(14.dp))
-            .background(White)
+            .background(DateRoadTheme.colors.white)
     ) {
         Column(modifier = Modifier.padding(start = 14.dp, top = 18.dp, bottom = 14.dp)) {
-            Text(text = "$nickname 님의 포인트", color = Gray400, style = DateRoadTheme.typography.bodyMed13)
+            Text(text = stringResource(id = R.string.point_box_nickname, nickname), color = DateRoadTheme.colors.gray400, style = DateRoadTheme.typography.bodyMed13)
             Spacer(modifier = Modifier.height(9.dp))
-            Text(text = "$point P", color = Black, style = DateRoadTheme.typography.titleExtra24)
+            Text(text = stringResource(id = R.string.point_box_point, point), color = DateRoadTheme.colors.black, style = DateRoadTheme.typography.titleExtra24)
         }
 
         Row(
             modifier = Modifier
                 .align(Alignment.BottomEnd)
                 .padding(end = 10.dp, bottom = 11.dp),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
+            horizontalArrangement = Arrangement.Center
         ) {
-            Text(text = stringResource(id = R.string.my_page_point_card_text), style = DateRoadTheme.typography.bodyMed13, color = Gray400)
-            Spacer(modifier = Modifier.width(4.dp))
-            Icon(painter = painterResource(id = R.drawable.ic_my_page_point_record_arrow), contentDescription = null, tint = Gray400)
+            Row(
+                modifier = Modifier.padding(horizontal = 14.dp, vertical = 11.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = stringResource(id = R.string.my_page_point_card_text), style = DateRoadTheme.typography.bodyMed13, color = DateRoadTheme.colors.gray400)
+                Spacer(modifier = Modifier.width(10.dp))
+                Icon(painter = painterResource(id = R.drawable.ic_my_page_point_record_arrow), contentDescription = null, tint = DateRoadTheme.colors.gray400)
+            }
         }
     }
 }
--- app/src/main/java/org/sopt/dateroad/presentation/ui/component/pointbox/DateRoadPointHistoryPointBox.kt
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/component/pointbox/DateRoadPointHistoryPointBox.kt
@@ -3,32 +3,34 @@ package org.sopt.dateroad.presentation.ui.component.pointbox
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import org.sopt.dateroad.R
 import org.sopt.dateroad.ui.theme.DateRoadTheme
-import org.sopt.dateroad.ui.theme.DeepPurple
-import org.sopt.dateroad.ui.theme.White
 
 @Composable
 fun DateRoadPointHistoryPointBox(nickname: String, point: Int) {
     Column(
         modifier = Modifier
-            .size(width = 328.dp, height = 90.dp)
+            .height(90.dp)
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
             .clip(RoundedCornerShape(14.dp))
-            .background(DeepPurple)
+            .background(DateRoadTheme.colors.deepPurple)
     ) {
         Column(modifier = Modifier.padding(start = 16.dp, top = 16.dp, bottom = 14.dp)) {
-            Text(text = "$nickname 님의 포인트", color = White, style = DateRoadTheme.typography.bodyMed13)
+            Text(text = stringResource(id = R.string.point_box_nickname, nickname), color = DateRoadTheme.colors.white, style = DateRoadTheme.typography.bodyMed13)
             Spacer(modifier = Modifier.height(11.dp))
-            Text(text = "$point P", color = White, style = DateRoadTheme.typography.titleExtra24)
+            Text(text = stringResource(id = R.string.point_box_point, point), color = DateRoadTheme.colors.white, style = DateRoadTheme.typography.titleExtra24)
         }
     }
 }
