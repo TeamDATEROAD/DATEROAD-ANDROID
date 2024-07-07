e7339433a857f3458b81e0233ef9d01042fd4cf7
t1nm1ksun <minsuk07672@naver.com>
2024-07-07 23:24:16 +0900

    [init] 파일 생성

---
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/component/placecard/DateRoadPlaceCard.kt
@@ -0,0 +1,9 @@
+package org.sopt.dateroad.presentation.ui.component.placecard
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DateRoadPlaceCard(modifier: Modifier = Modifier) {
+    
+}
