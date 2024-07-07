656a1c9cbc25f72e8eb58e4331692c68c4625833
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-07-08 00:42:29 +0900

    [chore] #9 DateRoadBasicButton

--- app/src/main/java/org/sopt/dateroad/presentation/ui/component/button/DateRoadBasicButton.kt
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/component/button/DateRoadBasicButton.kt
@@ -1,17 +1,9 @@
 package org.sopt.dateroad.presentation.ui.component.button
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.sopt.dateroad.ui.theme.DATEROADTheme
@@ -21,38 +13,38 @@ import org.sopt.dateroad.ui.theme.DateRoadTheme
 fun DateRoadBasicButton(
     modifier: Modifier = Modifier,
     isEnabled: Boolean = true,
-    enabledBackgroundColor: Color = DateRoadTheme.colors.deepPurple,
-    enabledContentColor: Color = DateRoadTheme.colors.white,
-    disabledBackgroundColor: Color = DateRoadTheme.colors.gray200,
-    disabledContentColor: Color = DateRoadTheme.colors.gray400
+    textContent: String,
+    onClick: () -> Unit = {},
 ) {
-    val backgroundColor = if (isEnabled) enabledBackgroundColor else disabledBackgroundColor
-    val contentColor = if (isEnabled) enabledContentColor else disabledContentColor
-
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .clip(RoundedCornerShape(14.dp))
-            .background(backgroundColor)
-            .padding(vertical = 16.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Center
-    ) {
-        Text(
-            text = "프로필 등록하기",
-            style = DateRoadTheme.typography.bodyBold15,
-            color = contentColor,
-            modifier = Modifier.align(Alignment.CenterVertically)
-        )
-    }
+    DateRoadFilledButton(
+        modifier = modifier.fillMaxWidth(),
+        isEnabled = isEnabled,
+        textContent = textContent,
+        textStyle = DateRoadTheme.typography.bodyBold15,
+        enabledBackgroundColor = DateRoadTheme.colors.deepPurple,
+        enabledTextColor = DateRoadTheme.colors.white,
+        disabledBackgroundColor = DateRoadTheme.colors.gray200,
+        disabledTextColor = DateRoadTheme.colors.gray400,
+        cornerRadius = 14.dp,
+        paddingHorizontal = 0.dp,
+        paddingVertical = 16.dp,
+        onClick = onClick
+    )
 }
 
 @Preview
 @Composable
 fun DateRoadBasicButtonPreview() {
     DATEROADTheme {
-        DateRoadBasicButton(
-            isEnabled = true
-        )
+        Column {
+            DateRoadBasicButton(
+                isEnabled = true,
+                textContent = "프로필 등록하기",
+            )
+            DateRoadBasicButton(
+                isEnabled = false,
+                textContent = "프로필 등록하기",
+            )
+        }
     }
 }
