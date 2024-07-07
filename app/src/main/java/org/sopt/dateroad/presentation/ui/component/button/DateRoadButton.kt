2885ff14e4aaef6fa1c143c8e7fbe73880b77620
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-07-08 00:45:01 +0900

    [chore] #9 Alignment 속성 제거

--- app/src/main/java/org/sopt/dateroad/presentation/ui/component/button/DateRoadButton.kt
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/component/button/DateRoadButton.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -33,7 +32,6 @@ fun DateRoadButton(
             .clickable(onClick = onClick)
             .border(width = borderWidth, color = borderColor, shape = RoundedCornerShape(cornerRadius))
             .padding(horizontal = paddingHorizontal, vertical = paddingVertical),
-        contentAlignment = Alignment.Center
     ) {
         content()
     }
