9da0dcc3e091f8926d1114ef4c59233f46721af2
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-07-07 19:55:11 +0900

    [feat] #13 DateRoadTag

---
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/component/tag/DateRoadTag.kt
@@ -0,0 +1,27 @@
+package org.sopt.dateroad.presentation.ui.component.tag
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import org.sopt.dateroad.presentation.type.TagType
+
+@Composable
+fun DateRoadTag(
+    modifier: Modifier = Modifier,
+    tagType: TagType,
+    content: @Composable () -> Unit,
+) {
+    Box(
+        modifier = modifier
+            .clip(RoundedCornerShape(tagType.roundedCornerShape.dp))
+            .background(color = tagType.backgroundColor)
+            .padding(horizontal = tagType.paddingHorizontal.dp, vertical = tagType.paddingVertical.dp)
+    ) {
+        content()
+    }
+}
