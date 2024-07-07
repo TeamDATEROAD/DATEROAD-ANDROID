c6a016bf2cde1d35775a63455efcd8f0857f27cf
jihyunniiii <jita1125@naver.com>
2024-07-07 23:21:52 +0900

    [feat] #27 DateRoadNumberPicker

---
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/component/numberpicker/PickerState.kt
@@ -0,0 +1,16 @@
+package org.sopt.dateroad.presentation.ui.component.numberpicker
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+class PickerState {
+    var selectedItem by mutableStateOf("")
+}
+
+@Composable
+fun rememberPickerState() = remember {
+    PickerState()
+}
