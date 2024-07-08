e64a390dbf1a6364a93535381194d61ca59be2f4
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-07-09 01:50:26 +0900

    [feat] #48 DateRoadCourseColumnCard

---
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/component/item/DateRoadCourseCardColumn.kt
@@ -0,0 +1,112 @@
+package org.sopt.dateroad.presentation.ui.component.item
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import org.sopt.dateroad.R
+import org.sopt.dateroad.domain.model.Course
+import org.sopt.dateroad.presentation.type.TagType
+import org.sopt.dateroad.presentation.ui.component.tag.DateRoadImageTag
+import org.sopt.dateroad.ui.theme.DateRoadTheme
+
+@Composable
+fun DateRoadCourseColumnCard(
+    course: Course
+) {
+    Column(
+        modifier = Modifier
+            .background(DateRoadTheme.colors.white)
+
+    ) {
+        Text(
+            text = course.city,
+            style = DateRoadTheme.typography.bodyMed13,
+            color = DateRoadTheme.colors.white,
+            modifier = Modifier
+                .clip(RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp))
+                .background(DateRoadTheme.colors.mediumPurple)
+                .padding(vertical = 2.dp, horizontal = 13.dp)
+        )
+        Box(
+            modifier = Modifier
+                .size(230.dp)
+        ) {
+            AsyncImage(
+                model = ImageRequest.Builder(context = LocalContext.current)
+                    .data(course.url)
+                    .crossfade(true)
+                    .build(),
+                placeholder = null,
+                contentDescription = null,
+                modifier = Modifier
+                    .matchParentSize()
+                    .clip(RoundedCornerShape(topStart = 0.dp, topEnd = 14.dp, bottomEnd = 14.dp, bottomStart = 14.dp))
+            )
+            DateRoadImageTag(
+                textContent = course.like.toString(),
+                imageContent = R.drawable.ic_tag_heart,
+                tagContentType = TagType.HEART,
+                modifier = Modifier
+                    .align(Alignment.BottomStart)
+                    .padding(start = 5.dp, bottom = 5.dp, end = 5.dp)
+            )
+        }
+        Text(
+            text = course.title,
+            style = DateRoadTheme.typography.bodyBold17,
+            color = DateRoadTheme.colors.black,
+            modifier = Modifier
+                .padding(top = 6.dp, bottom = 8.dp),
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
+        Row {
+            DateRoadImageTag(
+                textContent = "${course.cost}만원 초과",
+                imageContent = R.drawable.ic_all_money_12,
+                tagContentType = TagType.MONEY
+            )
+            Spacer(modifier = Modifier.size(6.dp))
+            DateRoadImageTag(
+                textContent = "${course.duration}시간",
+                imageContent = R.drawable.ic_all_clock_12,
+                tagContentType = TagType.TIME
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DateRoadCourseColumnCardPreview() {
+    Column {
+        DateRoadCourseColumnCard(
+            course = Course(
+                id = 1,
+                url = "https://avatars.githubusercontent.com/u/103172971?v=4",
+                openedAt = "2023-01-01",
+                city = "건대/성수/왕십리",
+                title = "여기 야키니쿠 꼭 먹으러 가세요\n하지만 일본에 있습니다.",
+                cost = 10,
+                duration = 10,
+                like = 999
+            )
+        )
+    }
+}
