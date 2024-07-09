38956d2356d84c3eb34d45d106c28240e8ed748c
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-07-09 17:39:27 +0900

    [add] #58 DateCard

---
+++ app/src/main/java/org/sopt/dateroad/domain/model/DateCard.kt
@@ -0,0 +1,17 @@
+package org.sopt.dateroad.domain.model
+
+import org.sopt.dateroad.presentation.type.DateTagType
+
+data class DateCard(
+    val dateId: Long,
+    val title: String,
+    val month: String,
+    val day: String,
+    val city: String,
+    val tags: List<TimelineTag>,
+    val dDay: String
+)
+
+data class TimelineTag(
+    val timelineTag: DateTagType
+)
