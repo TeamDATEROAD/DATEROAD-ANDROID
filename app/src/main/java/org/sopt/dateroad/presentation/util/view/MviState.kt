c3406da8e223bf549f219df3826c13d71a6364fb
jihyunniiii <jita1125@naver.com>
2024-07-10 00:26:26 +0900

    [rename] #64 mviState -> LoadState

--- app/src/main/java/org/sopt/dateroad/presentation/util/view/MviState.kt
+++
@@ -1,8 +0,0 @@
-package org.sopt.dateroad.presentation.util.view
-
-sealed class MviState<out T> {
-    data object Idle : MviState<Nothing>()
-    data object Loading : MviState<Nothing>()
-    data class Success<T>(val data: T) : MviState<T>()
-    data class Error<T>(val message: String?) : MviState<T>()
-}
