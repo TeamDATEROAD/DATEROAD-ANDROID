f78352e85777bb5a43abd6b66bcdd3faca66b6e0
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-07-11 04:43:13 +0900

    [feat] #59 MyCourseViewModel

--- app/src/main/java/org/sopt/dateroad/presentation/ui/mycourse/MyCourseViewModel.kt
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/mycourse/MyCourseViewModel.kt
@@ -3,6 +3,7 @@ package org.sopt.dateroad.presentation.ui.mycourse
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import org.sopt.dateroad.domain.model.Course
+import org.sopt.dateroad.presentation.type.MyCourseType
 import org.sopt.dateroad.presentation.util.base.BaseViewModel
 import org.sopt.dateroad.presentation.util.view.LoadState
 
@@ -12,14 +13,92 @@ class MyCourseViewModel @Inject constructor() : BaseViewModel<MyCourseContract.M
 
     override suspend fun handleEvent(event: MyCourseContract.MyCourseEvent) {
         when (event) {
-            is MyCourseContract.MyCourseEvent.FetchMyCourses -> fetchMyCourses()
+            is MyCourseContract.MyCourseEvent.FetchMyCourseEnroll -> fetchMyCourseEnroll()
+            is MyCourseContract.MyCourseEvent.FetchMyCourseRead -> fetchMyCourseRead()
+            is MyCourseContract.MyCourseEvent.SetMyCourseType -> setState { copy(myCourseType = event.myCourseType) }
         }
     }
 
-    private fun fetchMyCourses() {
+    private fun fetchMyCourseRead() {
         setState {
             copy(
                 loadState = LoadState.Success,
+                myCourseType = MyCourseType.READ,
+                courses = listOf(
+                    Course(
+                        id = 1,
+                        url = "https://avatars.githubusercontent.com/u/103172971?v=4",
+                        city = "건대/성수/왕십리",
+                        title = "여기 야키니쿠 꼭 먹으러 가세요\n하지만 일본에 있습니다.",
+                        cost = "10만원 초과",
+                        duration = "10시간",
+                        like = "99999"
+                    ),
+                    Course(
+                        id = 2,
+                        url = "https://avatars.githubusercontent.com/u/103172971?v=4",
+                        city = "부천",
+                        title = "여기 야키니쿠 꼭 먹으러 가세요.",
+                        cost = "10만원 초과",
+                        duration = "10시간",
+                        like = "999"
+                    ),
+                    Course(
+                        id = 3,
+                        url = "https://avatars.githubusercontent.com/u/103172971?v=4",
+                        city = "건대/성수/왕십리",
+                        title = "여기 야키니쿠 꼭 먹으러 가세요\n하지만 일본에 있습니다.하지만 일본에 있습니다.하지만 일본에 있습니다.하지만 일본에 있습니다.하지만 일본에 있습니다.",
+                        cost = "10만원 초과",
+                        duration = "10시간",
+                        like = "999"
+                    ),
+                    Course(
+                        id = 4,
+                        url = "https://avatars.githubusercontent.com/u/103172971?v=4",
+                        city = "건대/성수/왕십리",
+                        title = "여기 야키니쿠 꼭 먹으러 가세요\n하지만 일본에 있습니다.",
+                        cost = "10만원 초과",
+                        duration = "10시간",
+                        like = "999"
+                    ),
+                    Course(
+                        id = 5,
+                        url = "https://avatars.githubusercontent.com/u/103172971?v=4",
+                        city = "건대/성수/왕십리",
+                        title = "여기 야키니쿠 꼭 먹으러 가세요\n하지만 일본에 있습니다.",
+                        cost = "10만원 초과",
+                        duration = "10시간",
+                        like = "999"
+                    ),
+                    Course(
+                        id = 6,
+                        url = "https://avatars.githubusercontent.com/u/103172971?v=4",
+                        city = "건대/성수/왕십리",
+                        title = "여기 야키니쿠 꼭 먹으러 가세요\n하지만 일본에 있습니다.",
+                        cost = "10만원 초과",
+                        duration = "10시간",
+                        like = "999"
+                    ),
+                    Course(
+                        id = 7,
+                        url = "https://avatars.githubusercontent.com/u/103172971?v=4",
+                        city = "건대/성수/왕십리",
+                        title = "여기 야키니쿠 꼭 먹으러 가세요\n하지만 일본에 있습니다.",
+                        cost = "10만원 초과",
+                        duration = "10시간",
+                        like = "999"
+                    )
+
+                )
+            )
+        }
+    }
+
+    private fun fetchMyCourseEnroll() {
+        setState {
+            copy(
+                loadState = LoadState.Success,
+                myCourseType = MyCourseType.ENROLL,
                 courses = listOf(
                     Course(
                         id = 1,
