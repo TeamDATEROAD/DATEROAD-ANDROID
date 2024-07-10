8cd5b515c9670426a3a6364244efaffe98584328
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-07-11 04:45:23 +0900

    [feat] #59 MyCourseNavigation route 설정

--- app/src/main/java/org/sopt/dateroad/presentation/ui/mycourse/navigation/MyCourseNavigation.kt
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/mycourse/navigation/MyCourseNavigation.kt
@@ -3,23 +3,40 @@ package org.sopt.dateroad.presentation.ui.mycourse.navigation
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavOptions
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import org.sopt.dateroad.presentation.type.MyCourseType
 import org.sopt.dateroad.presentation.ui.mycourse.MyCourseRoute
+import org.sopt.dateroad.presentation.ui.mycourse.navigation.MyCoursesRoute.BASE_TYPE
 
-fun NavController.navigateToMyCourses(navOptions: NavOptions) {
-    navigate(route = MyCoursesRoute.ROUTE)
+fun NavController.navigateToMyCourses(myCourseType: MyCourseType) {
+    this.navigate(route = MyCoursesRoute.route(myCourseType = myCourseType))
 }
 
 fun NavGraphBuilder.myCoursesGraph(
     padding: PaddingValues,
     popBackStack: () -> Unit
 ) {
-    composable(route = MyCoursesRoute.ROUTE) {
-        MyCourseRoute(padding = padding, popBackStack = popBackStack)
+    composable(
+        route = MyCoursesRoute.ROUTE_WITH_ARGUMENT,
+        arguments = listOf(
+            navArgument(BASE_TYPE) {
+                type = NavType.StringType
+            }
+        )
+    ) { backStackEntry ->
+        val myCourseType = backStackEntry.arguments?.getString(BASE_TYPE)?.let {
+            MyCourseType.valueOf(it)
+        } ?: MyCourseType.ENROLL
+
+        MyCourseRoute(padding = padding, popBackStack = popBackStack, myCourseType = myCourseType)
     }
 }
 
 object MyCoursesRoute {
-    const val ROUTE = "myCourses"
+    const val BASE_ROUTE = "myCourses"
+    const val ROUTE_WITH_ARGUMENT = "$BASE_ROUTE/{myCourseType}"
+    const val BASE_TYPE = "myCourseType"
+    fun route(myCourseType: MyCourseType) = "$BASE_ROUTE/${myCourseType.name}"
 }
