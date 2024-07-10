38253a5b5da7bd792f24057151864913ce9f64bf
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-07-11 04:42:25 +0900

    [feat] #59 MyCourseScreen 뷰 구현

--- app/src/main/java/org/sopt/dateroad/presentation/ui/mycourse/MyCourseScreen.kt
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/mycourse/MyCourseScreen.kt
@@ -1,34 +1,30 @@
 package org.sopt.dateroad.presentation.ui.mycourse
 
-import androidx.annotation.DrawableRes
-import androidx.compose.foundation.Image
+import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.flowWithLifecycle
 import org.sopt.dateroad.R
 import org.sopt.dateroad.domain.model.Course
 import org.sopt.dateroad.presentation.type.EmptyViewType
+import org.sopt.dateroad.presentation.type.MyCourseType
+import org.sopt.dateroad.presentation.ui.component.emptyview.DateRoadEmptyView
 import org.sopt.dateroad.presentation.ui.component.item.DateRoadCourseCard
 import org.sopt.dateroad.presentation.ui.component.topbar.DateRoadBasicTopBar
 import org.sopt.dateroad.presentation.util.view.LoadState
@@ -39,33 +35,36 @@ import org.sopt.dateroad.ui.theme.DateRoadTheme
 fun MyCourseRoute(
     padding: PaddingValues,
     viewModel: MyCourseViewModel = hiltViewModel(),
-    popBackStack: () -> Unit
+    popBackStack: () -> Unit,
+    myCourseType: MyCourseType
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
     val lifecycleOwner = LocalLifecycleOwner.current
 
     LaunchedEffect(Unit) {
-        viewModel.setEvent(MyCourseContract.MyCourseEvent.FetchMyCourses)
+        viewModel.setEvent(MyCourseContract.MyCourseEvent.SetMyCourseType(myCourseType = myCourseType))
+        Log.e("mycourse", myCourseType.toString())
     }
 
     LaunchedEffect(viewModel.sideEffect, lifecycleOwner) {
         viewModel.sideEffect.flowWithLifecycle(lifecycle = lifecycleOwner.lifecycle)
             .collect { myCourseSideEffect ->
                 when (myCourseSideEffect) {
-                    is MyCourseContract.MyCourseSideEffect.ShowError -> {
-                    }
+                    is MyCourseContract.MyCourseSideEffect.PopBackStack -> popBackStack()
                 }
             }
+    }
+
+    when (uiState.myCourseType) {
+        MyCourseType.ENROLL -> viewModel.setEvent(MyCourseContract.MyCourseEvent.FetchMyCourseEnroll)
+        MyCourseType.READ -> viewModel.setEvent(MyCourseContract.MyCourseEvent.FetchMyCourseRead)
     }
 
     when (uiState.loadState) {
         LoadState.Success -> {
             MyCourseScreen(
                 padding = padding,
-                topBarTitle = "내가 등록한 코스",
-                emptyImage = R.drawable.ic_empty_past, // 적절한 리소스로 대체
-                emptyContent = "등록한 코스가 없습니다.",
-                courses = uiState.courses,
+                myCourseUiState = uiState,
                 onIconClick = popBackStack
             )
         }
@@ -76,12 +75,10 @@ fun MyCourseRoute(
 
 @Composable
 fun MyCourseScreen(
-    padding: PaddingValues = PaddingValues(0.dp),
-    topBarTitle: String,
-    @DrawableRes emptyImage: Int,
-    emptyContent: String,
-    courses: List<Course>?,
+    padding: PaddingValues,
+    myCourseUiState: MyCourseContract.MyCourseUiState = MyCourseContract.MyCourseUiState(),
     onIconClick: () -> Unit
+
 ) {
     Column(
         modifier = Modifier
@@ -90,41 +87,31 @@ fun MyCourseScreen(
             .background(color = DateRoadTheme.colors.white)
     ) {
         DateRoadBasicTopBar(
-            title = topBarTitle,
+            title = stringResource(id = myCourseUiState.myCourseType.topBarTitleRes),
             iconLeftResource = R.drawable.ic_top_bar_back_white,
             backGroundColor = DateRoadTheme.colors.white,
             onIconClick = onIconClick
         )
-        if (courses.isNullOrEmpty()) {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-            ) {
-                Spacer(modifier = Modifier.height(79.dp))
-                Image(
-                    painter = painterResource(id = emptyImage),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                )
-                Text(
-                    text = emptyContent,
-                    style = DateRoadTheme.typography.titleBold18,
-                    color = DateRoadTheme.colors.gray500,
-                    fontSize = 20.sp,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 16.dp),
-                    textAlign = TextAlign.Center
-                )
-            }
-        } else {
-            LazyColumn {
-                items(courses) { course ->
-                    DateRoadCourseCard(
-                        course = course
-                    )
+        LazyColumn {
+            if (myCourseUiState.courses.isEmpty()) {
+                item {
+                    Box(
+                        modifier = Modifier
+                            .fillParentMaxSize()
+                    ) {
+                        DateRoadEmptyView(
+                            emptyViewType = when (myCourseUiState.myCourseType) {
+                                MyCourseType.ENROLL -> EmptyViewType.MY_COURSE_ENROLL
+                                MyCourseType.READ -> EmptyViewType.MY_COURSE_READ
+                            }
+                        )
+                    }
                 }
+            }
+            items(myCourseUiState.courses) { course ->
+                DateRoadCourseCard(
+                    course = course
+                )
             }
         }
     }
@@ -147,10 +134,7 @@ fun MyCourseScreenPreview() {
 
     DATEROADTheme {
         MyCourseScreen(
-            topBarTitle = "내가 등록한 코스",
-            emptyImage = EmptyViewType.MY_COURSE_ENROLL.imageRes,
-            emptyContent = "등록한 코스가 없습니다.",
-            courses = sampleCourses,
+            padding = PaddingValues(0.dp),
             onIconClick = {}
         )
     }
