5e528434f6929c14b9f1895fbf47f4ffdd269729
jihyunniiii <jita1125@naver.com>
2024-07-10 18:07:02 +0900

    [chore] #57 ktlintFormat

--- app/src/main/java/org/sopt/dateroad/presentation/ui/enroll/component/EnrollPlaceInsertBar.kt
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/enroll/component/EnrollPlaceInsertBar.kt
@@ -3,10 +3,8 @@ package org.sopt.dateroad.presentation.ui.enroll.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
