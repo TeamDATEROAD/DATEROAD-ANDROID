cf30820107ba4b10503b8dfd44dde57817927368
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-07-11 13:38:30 +0900

    [feat] #65 TimelineScreen TopBar 구현

--- app/src/main/java/org/sopt/dateroad/presentation/ui/timeline/TimelineScreen.kt
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/timeline/TimelineScreen.kt
@@ -1,8 +1,12 @@
 package org.sopt.dateroad.presentation.ui.timeline
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,29 +15,53 @@ import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import org.sopt.dateroad.presentation.ui.component.button.DateRoadImageButton
+import org.sopt.dateroad.presentation.ui.component.topbar.DateRoadCourseTopBar
 import org.sopt.dateroad.ui.theme.DATEROADTheme
+import org.sopt.dateroad.ui.theme.DateRoadTheme
 
 @Composable
 fun TimelineRoute(
-    padding: PaddingValues
+    padding: PaddingValues,
 ) {
-    TimelineScreen(padding)
+    TimelineScreen(
+        padding,
+    )
 }
 
 @Composable
 fun TimelineScreen(
-    padding: PaddingValues
+    padding: PaddingValues,
 ) {
     Column(
         modifier = Modifier
-            .padding(padding)
-            .fillMaxSize()
+            .fillMaxWidth()
+            .background(color = DateRoadTheme.colors.white)
     ) {
-        Text(
-            text = "TimelineScreen",
-            fontSize = 30.sp,
-            fontWeight = Bold
+        DateRoadCourseTopBar(
+            title = "데이트 일정",
+            buttonContent = {
+                DateRoadImageButton(
+                    isEnabled = true,
+                    onClick = {},
+                    cornerRadius = 14.dp,
+                    paddingHorizontal = 16.dp,
+                    paddingVertical = 8.dp
+                )
+            }
         )
+        Spacer(modifier = Modifier.height(20.dp))
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+        ) {
+            Text(
+                text = "TimelineScreen",
+                fontSize = 30.sp,
+                fontWeight = Bold
+            )
+        }
     }
 }
 
@@ -41,6 +69,8 @@ fun TimelineScreen(
 @Composable
 fun TimelineScreenPreview() {
     DATEROADTheme {
-        TimelineScreen(padding = PaddingValues(0.dp))
+        TimelineScreen(
+            padding = PaddingValues(0.dp),
+        )
     }
 }
