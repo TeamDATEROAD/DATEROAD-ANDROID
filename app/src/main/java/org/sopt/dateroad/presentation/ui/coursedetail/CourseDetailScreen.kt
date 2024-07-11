782bfd171185c0cd8c9f730718163f8670a2ff08
t1nm1ksun <minsuk07672@naver.com>
2024-07-12 06:14:28 +0900

    [chore] #78 isMyCourse, isAccess 분기처리

--- app/src/main/java/org/sopt/dateroad/presentation/ui/coursedetail/CourseDetailScreen.kt
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/coursedetail/CourseDetailScreen.kt
@@ -1,32 +1,29 @@
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInParent
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.rememberPagerState
-import kotlinx.coroutines.launch
 import org.sopt.dateroad.R
 import org.sopt.dateroad.domain.model.Course
 import org.sopt.dateroad.domain.model.Place
@@ -34,19 +31,26 @@ import org.sopt.dateroad.presentation.type.ChipType
 import org.sopt.dateroad.presentation.type.DateTagType
 import org.sopt.dateroad.presentation.type.PlaceCardType
 import org.sopt.dateroad.presentation.type.TagType
+import org.sopt.dateroad.presentation.type.TwoButtonDialogWithDescriptionType
+import org.sopt.dateroad.presentation.ui.component.bottomsheet.DateRoadBasicBottomSheet
 import org.sopt.dateroad.presentation.ui.component.button.DateRoadFilledButton
 import org.sopt.dateroad.presentation.ui.component.button.DateRoadImageButton
 import org.sopt.dateroad.presentation.ui.component.chip.DateRoadImageChip
+import org.sopt.dateroad.presentation.ui.component.dialog.DateRoadTwoButtonDialogWithDescription
 import org.sopt.dateroad.presentation.ui.component.placecard.DateRoadPlaceCard
 import org.sopt.dateroad.presentation.ui.component.tag.DateRoadImageTag
 import org.sopt.dateroad.presentation.ui.component.tag.DateRoadTextTag
 import org.sopt.dateroad.presentation.ui.coursedetail.component.CourseDetailInfoBar
 import org.sopt.dateroad.ui.theme.DateRoadTheme
 
-
 @OptIn(ExperimentalPagerApi::class)
 @Composable
-fun CourseDetailScreen(course: Course, places: List<Place>, tags: List<DateTagType>) {
+fun CourseDetailScreen(course: Course, places: List<Place>, tags: List<DateTagType>, isMyCourse: Boolean, isAccess: Boolean) {
+    var isBottomSheetOpen by rememberSaveable { mutableStateOf(false) }
+    var showTwoButtonDialogWithDescription = remember { mutableStateOf(false) }
+
+    var contentOffsetY by remember { mutableStateOf(0f) }
+    val density = LocalDensity.current.density
     val imageList = listOf(
         R.drawable.img_course_detail_dummy,
         R.drawable.img_course_detail_dummy,
@@ -86,14 +90,16 @@ fun CourseDetailScreen(course: Course, places: List<Place>, tags: List<DateTagTy
                             .padding(top = 5.dp)
                     )
 
-                    Image(
-                        painter = painterResource(id = R.drawable.btn_course_detail_more_white),
-                        contentDescription = null,
-                        modifier = Modifier
-                            .clickable { }
-                            .padding(top = 5.dp)
-                            .align(Alignment.TopEnd)
-                    )
+                    if (isMyCourse) {
+                        Image(
+                            painter = painterResource(id = R.drawable.btn_course_detail_more_white),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .clickable { isBottomSheetOpen = true }
+                                .padding(top = 5.dp)
+                                .align(Alignment.TopEnd)
+                        )
+                    }
 
                     DateRoadImageTag(
                         textContent = "5",
@@ -120,11 +126,20 @@ fun CourseDetailScreen(course: Course, places: List<Place>, tags: List<DateTagTy
                         .background(DateRoadTheme.colors.white)
                         .padding(horizontal = 16.dp)
                         .padding(top = 18.dp)
+                        .onGloballyPositioned { coordinates ->
+                            contentOffsetY = coordinates.positionInParent().y
+                        }
                 ) {
                     Text(
                         text = "2024년 6월 27일 방문",
                         style = DateRoadTheme.typography.bodySemi15,
                         color = DateRoadTheme.colors.gray400
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(
+                        text = "5년차 장기연애 커플이 보장하는 성수동 당일치기 데이트 코스",
+                        style = DateRoadTheme.typography.titleExtra24,
+                        color = DateRoadTheme.colors.black
                     )
                     Spacer(modifier = Modifier.height(16.dp))
                     CourseDetailInfoBar(
@@ -137,12 +152,6 @@ fun CourseDetailScreen(course: Course, places: List<Place>, tags: List<DateTagTy
                             duration = "10시간",
                             like = "999"
                         )
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Text(
-                        text = "5년차 장기연애 커플이 보장하는 성수동 당일치기 데이트 코스",
-                        style = DateRoadTheme.typography.titleExtra24,
-                        color = DateRoadTheme.colors.black
                     )
                     Spacer(modifier = Modifier.height(16.dp))
                     Text(
@@ -160,9 +169,11 @@ fun CourseDetailScreen(course: Course, places: List<Place>, tags: List<DateTagTy
             }
 
             items(places) { place ->
-                Column(modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp)) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp)
+                ) {
                     DateRoadPlaceCard(
                         placeCardType = PlaceCardType.COURSE_NORMAL,
                         place = place
@@ -171,9 +182,11 @@ fun CourseDetailScreen(course: Course, places: List<Place>, tags: List<DateTagTy
             }
 
             item {
-                Column(modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp)) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp)
+                ) {
                     Spacer(modifier = Modifier.height(16.dp))
                     Text(
                         text = "총 비용",
@@ -201,9 +214,11 @@ fun CourseDetailScreen(course: Course, places: List<Place>, tags: List<DateTagTy
                 }
             }
             item {
-                Column(modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp)) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp)
+                ) {
                     Row {
                         tags.forEach { tag ->
                             DateRoadImageChip(
@@ -215,53 +230,142 @@ fun CourseDetailScreen(course: Course, places: List<Place>, tags: List<DateTagTy
                             Spacer(modifier = Modifier.width(7.dp))
                         }
                     }
-                    Spacer(modifier = Modifier.height(118.dp))
+                    Spacer(modifier = Modifier.height(38.dp))
+                    if (!isMyCourse) {
+                        Spacer(modifier = Modifier.height(80.dp))
+                    }
                 }
             }
         }
 
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .align(Alignment.BottomCenter)
-                .padding(horizontal = 16.dp, vertical = 16.dp)
-        ) {
-            Row {
-                DateRoadImageButton(
-                    iconResId = R.drawable.ic_coures_detail_heart_default,
-                    enabledContentColor = DateRoadTheme.colors.deepPurple,
-                    disabledContentColor = DateRoadTheme.colors.gray200,
-                    enabledBackgroundColor = DateRoadTheme.colors.gray100,
-                    disabledBackgroundColor = DateRoadTheme.colors.gray100,
-                    isEnabled = true,
-                    onClick = {},
-                    cornerRadius = 14.dp,
-                    paddingHorizontal = 23.dp,
-                    paddingVertical = 18.dp
-                )
-                Spacer(modifier = Modifier.width(12.dp))
-                DateRoadFilledButton(
-                    modifier = Modifier.weight(1f),
-                    isEnabled = true,
-                    textContent = "코스 가져오기",
-                    onClick = {},
-                    textStyle = DateRoadTheme.typography.bodyBold15,
-                    enabledBackgroundColor = DateRoadTheme.colors.deepPurple,
-                    enabledTextColor = DateRoadTheme.colors.white,
-                    disabledBackgroundColor = DateRoadTheme.colors.gray200,
-                    disabledTextColor = DateRoadTheme.colors.gray400,
-                    cornerRadius = 10.dp,
-                    paddingHorizontal = 0.dp,
-                    paddingVertical = 17.dp
-                )
+        if (!isMyCourse) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.BottomCenter)
+                    .padding(horizontal = 16.dp, vertical = 16.dp)
+            ) {
+                Row {
+                    DateRoadImageButton(
+                        iconResId = R.drawable.ic_coures_detail_heart_default,
+                        enabledContentColor = DateRoadTheme.colors.deepPurple,
+                        disabledContentColor = DateRoadTheme.colors.gray200,
+                        enabledBackgroundColor = DateRoadTheme.colors.gray100,
+                        disabledBackgroundColor = DateRoadTheme.colors.gray100,
+                        isEnabled = true,
+                        onClick = {},
+                        cornerRadius = 14.dp,
+                        paddingHorizontal = 23.dp,
+                        paddingVertical = 18.dp
+                    )
+                    Spacer(modifier = Modifier.width(12.dp))
+                    DateRoadFilledButton(
+                        modifier = Modifier.weight(1f),
+                        isEnabled = true,
+                        textContent = "코스 가져오기",
+                        onClick = {showTwoButtonDialogWithDescription.value =true },
+                        textStyle = DateRoadTheme.typography.bodyBold15,
+                        enabledBackgroundColor = DateRoadTheme.colors.deepPurple,
+                        enabledTextColor = DateRoadTheme.colors.white,
+                        disabledBackgroundColor = DateRoadTheme.colors.gray200,
+                        disabledTextColor = DateRoadTheme.colors.gray400,
+                        cornerRadius = 10.dp,
+                        paddingHorizontal = 0.dp,
+                        paddingVertical = 17.dp
+                    )
+                }
+            }
+        }
+
+        if (!isAccess) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                DateRoadTheme.colors.white.copy(alpha = 1f)
+                            ),
+                            startY = contentOffsetY,
+                            endY = contentOffsetY + 220.dp.toPx(density)
+                        )
+                    )
+            ) {
+                Column(
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                ) {
+                    Spacer(modifier = Modifier.height(600.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_course_detail_is_not_access),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally)
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(
+                        text = "코스 정보가 궁금하신가요?",
+                        style = DateRoadTheme.typography.bodyBold17,
+                        color = DateRoadTheme.colors.black,
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = "50P로 코스를 확인해보세요",
+                        style = DateRoadTheme.typography.bodyBold17,
+                        color = DateRoadTheme.colors.black,
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    )
+                    Spacer(modifier = Modifier.height(24.dp))
+                    DateRoadFilledButton(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 52.dp)
+                            .align(Alignment.CenterHorizontally),
+                        isEnabled = true,
+                        textContent = "포인트로 코스 열람하기",
+                        onClick = {},
+                        textStyle = DateRoadTheme.typography.bodyBold15,
+                        enabledBackgroundColor = DateRoadTheme.colors.deepPurple,
+                        enabledTextColor = DateRoadTheme.colors.white,
+                        disabledBackgroundColor = DateRoadTheme.colors.gray200,
+                        disabledTextColor = DateRoadTheme.colors.gray400,
+                        cornerRadius = 14.dp,
+                        paddingHorizontal = 0.dp,
+                        paddingVertical = 16.dp
+                    )
+                }
             }
         }
     }
+    if (showTwoButtonDialogWithDescription.value) {
+        DateRoadTwoButtonDialogWithDescription(
+            twoButtonDialogWithDescriptionType = TwoButtonDialogWithDescriptionType.POINT_LACK,
+            onDismissRequest = { showTwoButtonDialogWithDescription.value = false },
+            onClickConfirm = { showTwoButtonDialogWithDescription.value = false },
+            onClickDismiss = { showTwoButtonDialogWithDescription.value = false }
+        )
+    }
+
+    DateRoadBasicBottomSheet(
+        isBottomSheetOpen = isBottomSheetOpen,
+        title = "데이트 코스 설정",
+        isButtonEnabled = false,
+        buttonText = "취소",
+        itemList = listOf(
+            "글 삭제" to { }
+        ),
+        onDismissRequest = { isBottomSheetOpen = !isBottomSheetOpen },
+        onButtonClick = { isBottomSheetOpen = !isBottomSheetOpen }
+    )
 }
 
 @Preview
 @Composable
 fun CourseDetailScreenPreview() {
+    var isBottomSheetOpen by rememberSaveable { mutableStateOf(false) }
+
     val course = Course(
         id = 1,
         url = "https://avatars.githubusercontent.com/u/103172971?v=4",
@@ -281,5 +385,26 @@ fun CourseDetailScreenPreview() {
         DateTagType.SHOPPING,
         DateTagType.INDOOR
     )
-    CourseDetailScreen(course = course, places = places, tags = tags)
+    CourseDetailScreen(
+        course = course,
+        places = places,
+        tags = tags,
+        isMyCourse = false,
+        isAccess = true
+    )
+
+    DateRoadBasicBottomSheet(
+        isBottomSheetOpen = isBottomSheetOpen,
+        title = "데이트 코스 설정",
+        isButtonEnabled = false,
+        buttonText = "취소",
+        itemList = listOf(
+            "글 삭제" to { isBottomSheetOpen = false }
+        ),
+        onDismissRequest = { isBottomSheetOpen = false }
+    )
+}
+
+private fun androidx.compose.ui.unit.Dp.toPx(density: Float): Float {
+    return this.value * density
 }
