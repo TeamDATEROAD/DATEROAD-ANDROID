552cf4336282c7d147cd0981b4925167202d78ec
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-07-12 17:44:38 +0900

    [chore] #74 sequence type 수정

--- app/src/main/java/org/sopt/dateroad/domain/model/Place.kt
+++ app/src/main/java/org/sopt/dateroad/domain/model/Place.kt
@@ -1,7 +1,7 @@
 package org.sopt.dateroad.domain.model
 
 data class Place(
-    val sequence: Int = 0,
+    val sequence: String = "",
     val title: String = "",
     val duration: String = ""
 )
