e6a2fd11c51e2c168c7e1e9c2d4014e06a2a0c81
t1nm1ksun <minsuk07672@naver.com>
2024-07-13 02:27:22 +0900

    [chore] #84 string 추출

--- app/src/main/java/org/sopt/dateroad/presentation/ui/pointguide/PointGuideScreen.kt
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/pointguide/PointGuideScreen.kt
@@ -9,13 +9,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.sopt.dateroad.R
 import org.sopt.dateroad.presentation.type.MyPagePointInfoType
 import org.sopt.dateroad.presentation.type.OnboardingType
 import org.sopt.dateroad.presentation.ui.component.mypage.DateRoadMyPagePointInfo
+import org.sopt.dateroad.presentation.ui.component.partialcolortext.PartialColorText
 import org.sopt.dateroad.presentation.ui.component.topbar.DateRoadBasicTopBar
 import org.sopt.dateroad.ui.theme.DateRoadTheme
 
@@ -30,16 +32,14 @@ fun ProfileGuideRoute(
 @Composable
 fun PointGuideScreen(
     onIconClick: () -> Unit
-
 ) {
     Column(
         Modifier
             .fillMaxSize()
             .background(DateRoadTheme.colors.white)
-
     ) {
         DateRoadBasicTopBar(
-            title = "포인트 제도 소개",
+            title = stringResource(id = R.string.point_guide_top_bar),
             iconLeftResource = R.drawable.ic_top_bar_back_white,
             backGroundColor = DateRoadTheme.colors.white,
             onIconClick = onIconClick
@@ -50,17 +50,21 @@ fun PointGuideScreen(
                 .padding(horizontal = 16.dp)
         ) {
             Spacer(modifier = Modifier.height(17.dp))
-
-            Text(text = "데이트로드는 포인트로\n 데이트 코스 열람할 수 있어도.")
-            Spacer(modifier = Modifier.height(16.dp))
-
             Text(
-                text = "포인트는 데이트 코스 등록하면 얻을 수 있어요.",
+                text = PartialColorText(
+                    text = stringResource(id = R.string.point_guide_title),
+                    keywords = listOf("포인트", "데이트 코스"),
+                    color = DateRoadTheme.colors.deepPurple
+                ),
+                style = DateRoadTheme.typography.titleExtra20
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = stringResource(id = R.string.point_guide_description),
                 style = DateRoadTheme.typography.bodyMed15,
                 color = DateRoadTheme.colors.gray500
             )
             Spacer(modifier = Modifier.height(24.dp))
-
             DateRoadMyPagePointInfo(myPagePointInfoType = MyPagePointInfoType.FIRST)
             Spacer(modifier = Modifier.height(16.dp))
             DateRoadMyPagePointInfo(myPagePointInfoType = MyPagePointInfoType.SECOND)
@@ -70,4 +74,10 @@ fun PointGuideScreen(
             DateRoadMyPagePointInfo(myPagePointInfoType = MyPagePointInfoType.FOURTH)
         }
     }
+}
+
+@Preview
+@Composable
+fun PointGuideScreenPreview() {
+    PointGuideScreen(onIconClick = {})
 }
