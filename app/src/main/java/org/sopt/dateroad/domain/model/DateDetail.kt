f990e8aefa4895f92ffd020c214c440bf53acd3f
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-07-13 07:01:10 +0900

    [chore] #74 초기값 설정

--- app/src/main/java/org/sopt/dateroad/domain/model/DateDetail.kt
+++ app/src/main/java/org/sopt/dateroad/domain/model/DateDetail.kt
@@ -3,12 +3,12 @@ package org.sopt.dateroad.domain.model
 import org.sopt.dateroad.presentation.type.DateTagType
 
 data class DateDetail(
-    val dateId: Int,
-    val title: String,
-    val startAt: String,
-    val city: String,
-    val tags: List<DateTagType>,
-    val date: String,
-    val dday: String,
-    val places: List<Place>
+    val dateId: Int = 0,
+    val title: String = "",
+    val startAt: String = "",
+    val city: String = "",
+    val tags: List<DateTagType> = emptyList(),
+    val date: String = "",
+    val dday: String = "",
+    val places: List<Place> = emptyList()
 )
