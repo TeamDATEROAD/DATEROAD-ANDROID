46c8f2c95f84e099b3cbb80f2751b2c00e742a2c
t1nm1ksun <minsuk07672@naver.com>
2024-07-13 00:52:24 +0900

    [chore] #60 ktlint Format

--- app/src/main/java/org/sopt/dateroad/presentation/ui/onboarding/component/OnboardingIndicator.kt
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/onboarding/component/OnboardingIndicator.kt
@@ -11,7 +11,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import org.sopt.dateroad.ui.theme.DateRoadTheme
 
 @Composable
