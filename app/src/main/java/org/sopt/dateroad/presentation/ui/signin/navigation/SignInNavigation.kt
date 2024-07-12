66806c2ae35f97e223fefa670ee3462384dc00f9
t1nm1ksun <minsuk07672@naver.com>
2024-07-13 04:41:15 +0900

    [chore] #84 uiState 인자 적용

--- app/src/main/java/org/sopt/dateroad/presentation/ui/signin/navigation/SignInNavigation.kt
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/signin/navigation/SignInNavigation.kt
@@ -2,6 +2,7 @@ package org.sopt.dateroad.presentation.ui.signin.navigation
 
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import org.sopt.dateroad.presentation.ui.signin.SignInContract
 import org.sopt.dateroad.presentation.ui.signin.SignInRoute
 import org.sopt.dateroad.presentation.ui.signin.SignInViewModel
 
@@ -9,7 +10,8 @@ fun NavGraphBuilder.signInGraph(onSignIn: () -> Unit) {
     composable(route = SignInRoute.ROUTE) {
         SignInRoute(
             onSignIn = onSignIn,
-            viewModel = SignInViewModel()
+            viewModel = SignInViewModel(),
+            uiState = SignInContract.SignInUiState()
         )
     }
 }
