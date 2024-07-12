0d040c9d7d39197378bda2e199266533316496d3
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-07-13 04:25:07 +0900

    [chore] #82 ktlintFormat

--- app/src/main/java/org/sopt/dateroad/presentation/ui/navigator/component/MainNavHost.kt
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/navigator/component/MainNavHost.kt
@@ -13,7 +13,6 @@ import org.sopt.dateroad.presentation.ui.look.navigation.lookNavGraph
 import org.sopt.dateroad.presentation.ui.mycourse.navigation.myCoursesGraph
 import org.sopt.dateroad.presentation.ui.mypage.navigation.myPageNavGraph
 import org.sopt.dateroad.presentation.ui.navigator.MainNavigator
-import org.sopt.dateroad.presentation.ui.onboarding.navigation.onboardingNavGraph
 import org.sopt.dateroad.presentation.ui.pointhistory.navigation.pointHistoryGraph
 import org.sopt.dateroad.presentation.ui.profile.navigation.profileNavGraph
 import org.sopt.dateroad.presentation.ui.read.navigation.readNavGraph
