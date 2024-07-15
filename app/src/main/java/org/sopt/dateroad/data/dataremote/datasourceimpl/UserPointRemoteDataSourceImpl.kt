ae065a6d5d57d8813ff7b3a5dd6f798fe819f6f9
jihyunniiii <jita1125@naver.com>
2024-07-16 04:22:55 +0900

    [chore] #109 ktlintFormat

--- app/src/main/java/org/sopt/dateroad/data/dataremote/datasourceimpl/UserPointRemoteDataSourceImpl.kt
+++ app/src/main/java/org/sopt/dateroad/data/dataremote/datasourceimpl/UserPointRemoteDataSourceImpl.kt
@@ -1,11 +1,11 @@
 package org.sopt.dateroad.data.dataremote.datasourceimpl
 
+import javax.inject.Inject
 import org.sopt.dateroad.data.dataremote.datasource.UserPointRemoteDataSource
 import org.sopt.dateroad.data.dataremote.model.request.RequestUsePointDto
 import org.sopt.dateroad.data.dataremote.model.response.ResponsePointHistoryDto
 import org.sopt.dateroad.data.dataremote.model.response.ResponseUserPointDto
 import org.sopt.dateroad.data.dataremote.service.UserPointService
-import javax.inject.Inject
 
 class UserPointRemoteDataSourceImpl @Inject constructor(
     private val userPointService: UserPointService
--- app/src/main/java/org/sopt/dateroad/data/repositoryimpl/UserPointRepositoryImpl.kt
+++ app/src/main/java/org/sopt/dateroad/data/repositoryimpl/UserPointRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package org.sopt.dateroad.data.repositoryimpl
 
+import javax.inject.Inject
 import org.sopt.dateroad.data.dataremote.datasource.UserPointRemoteDataSource
 import org.sopt.dateroad.data.mapper.todata.toData
 import org.sopt.dateroad.data.mapper.todomain.toDomain
@@ -7,7 +8,6 @@ import org.sopt.dateroad.domain.model.PointHistory
 import org.sopt.dateroad.domain.model.UsePoint
 import org.sopt.dateroad.domain.model.UserPoint
 import org.sopt.dateroad.domain.repository.UserPointRepository
-import javax.inject.Inject
 
 class UserPointRepositoryImpl @Inject constructor(
     private val userPointRemoteDataSource: UserPointRemoteDataSource
