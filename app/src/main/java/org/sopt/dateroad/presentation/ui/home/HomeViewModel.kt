e67371002eae826c685fc74b3d292a3b811eefdd
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-07-16 19:50:29 +0900

    [chore] #105 HomeViewModel

--- app/src/main/java/org/sopt/dateroad/presentation/ui/home/HomeViewModel.kt
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/home/HomeViewModel.kt
@@ -39,16 +39,18 @@ class HomeViewModel @Inject constructor(
 
     fun fetchAdvertisements() {
         viewModelScope.launch {
-            setEvent(HomeContract.HomeEvent.FetchAdvertisements(loadState = LoadState.Loading, advertisements = emptyList()))
+            val currentAdvertisements = currentState.advertisements
+            setEvent(HomeContract.HomeEvent.FetchAdvertisements(loadState = LoadState.Loading, advertisements = currentAdvertisements))
             getAdvertisementsUseCase()
                 .onSuccess { advertisements ->
                     setEvent(HomeContract.HomeEvent.FetchAdvertisements(loadState = LoadState.Success, advertisements = advertisements))
                 }
                 .onFailure {
-                    setEvent(HomeContract.HomeEvent.FetchAdvertisements(loadState = LoadState.Error, advertisements = emptyList()))
+                    setEvent(HomeContract.HomeEvent.FetchAdvertisements(loadState = LoadState.Error, advertisements = currentAdvertisements))
                 }
         }
     }
+
 
     fun fetchLatestCourses() {
         setEvent(
