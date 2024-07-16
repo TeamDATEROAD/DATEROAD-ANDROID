c69e12e8a21f4e21dffac867324fcd930a21f0c8
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-07-17 00:03:42 +0900

    [chore] #114 conflict 해결

--- app/src/main/java/org/sopt/dateroad/presentation/ui/component/item/HomeAdvertisement.kt
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/component/item/HomeAdvertisement.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
