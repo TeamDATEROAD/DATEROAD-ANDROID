62496fab4bcf96d4854176e52fed7794f5068e01
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2024-07-17 16:29:24 +0900

    [chore] #105 ResponseAdvertisementsDto

--- app/src/main/java/org/sopt/dateroad/data/mapper/todomain/ResponseAdvertisementsDtoMapper.kt
+++ app/src/main/java/org/sopt/dateroad/data/mapper/todomain/ResponseAdvertisementsDtoMapper.kt
@@ -3,4 +3,4 @@ package org.sopt.dateroad.data.mapper.todomain
 import org.sopt.dateroad.data.dataremote.model.response.ResponseAdvertisementsDto
 import org.sopt.dateroad.domain.model.Advertisement
 
-fun ResponseAdvertisementsDto.toDomain(): List<Advertisement> = this.advertisementDtoResList.map { responseAdvertisementDto -> responseAdvertisementDto.toDomain() }
+fun ResponseAdvertisementsDto.toDomain(): List<Advertisement> = this.advertisements.map { responseAdvertisementDto -> responseAdvertisementDto.toDomain() }
