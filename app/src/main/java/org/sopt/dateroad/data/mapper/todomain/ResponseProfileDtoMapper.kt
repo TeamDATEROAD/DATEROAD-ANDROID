b49c19d1d6aa249dff4baa995f11c05688d54996
t1nm1ksun <minsuk07672@naver.com>
2024-07-17 19:58:17 +0900

    [chore] #121  ktlint Format

--- app/src/main/java/org/sopt/dateroad/data/mapper/todomain/ResponseProfileDtoMapper.kt
+++ app/src/main/java/org/sopt/dateroad/data/mapper/todomain/ResponseProfileDtoMapper.kt
@@ -1,7 +1,6 @@
 package org.sopt.dateroad.data.mapper.todomain
 
 import org.sopt.dateroad.data.dataremote.model.response.ResponseProfileDto
-import org.sopt.dateroad.data.dataremote.util.Point.POINT
 import org.sopt.dateroad.domain.model.Profile
 
 fun ResponseProfileDto.toDomain(): Profile = Profile(
