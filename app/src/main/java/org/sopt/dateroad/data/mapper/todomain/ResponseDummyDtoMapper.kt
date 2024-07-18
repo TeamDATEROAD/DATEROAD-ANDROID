58e697582af212f1485232e4c4c34110c79a0b7d
jihyunniiii <jita1125@naver.com>
2024-07-19 04:33:17 +0900

    [del] #143 delete dummy

--- app/src/main/java/org/sopt/dateroad/data/mapper/todomain/ResponseDummyDtoMapper.kt
+++
@@ -1,9 +0,0 @@
-package org.sopt.dateroad.data.mapper.todomain
-
-import org.sopt.dateroad.data.dataremote.model.response.ResponseDummiesDto
-import org.sopt.dateroad.domain.model.Dummy
-
-fun ResponseDummiesDto.ResponseDummyDto.toDomain(): Dummy = Dummy(
-    id = this.id,
-    email = this.email
-)
