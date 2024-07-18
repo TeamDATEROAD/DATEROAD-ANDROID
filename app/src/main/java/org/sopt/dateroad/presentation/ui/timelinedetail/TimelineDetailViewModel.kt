64dff5e98b5cd898f5c45b36b4d2d7b9ecd632ba
jihyunniiii <jita1125@naver.com>
2024-07-18 11:47:09 +0900

    [chore] #132 timelineDetail 뷰 서버통신 로직 수정

--- app/src/main/java/org/sopt/dateroad/presentation/ui/timelinedetail/TimelineDetailViewModel.kt
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/timelinedetail/TimelineDetailViewModel.kt
@@ -19,54 +19,35 @@ class TimelineDetailViewModel @Inject constructor(
 
     override suspend fun handleEvent(event: TimelineDetailContract.TimelineDetailEvent) {
         when (event) {
-            is TimelineDetailContract.TimelineDetailEvent.FetchTimelineDetail -> fetchDateDetail(event.dateId)
-            is TimelineDetailContract.TimelineDetailEvent.SetTimelineDetail -> setTimelineDetail(event.loadState, event.dateDetail)
+            is TimelineDetailContract.TimelineDetailEvent.SetTimelineDetail -> setState { copy(loadState = event.loadState, dateDetail = event.dateDetail) }
             is TimelineDetailContract.TimelineDetailEvent.SetShowDeleteBottomSheet -> setState { copy(showDeleteBottomSheet = event.showDeleteBottomSheet) }
             is TimelineDetailContract.TimelineDetailEvent.SetShowDeleteDialog -> setState { copy(showDeleteDialog = event.showDeleteDialog) }
             is TimelineDetailContract.TimelineDetailEvent.SetShowKakaoDialog -> setState { copy(showKakaoDialog = event.showKakaoDialog) }
-            is TimelineDetailContract.TimelineDetailEvent.SetSourceScreen -> setState { copy(sourceScreen = event.sourceScreen) }
-            is TimelineDetailContract.TimelineDetailEvent.DeleteDate -> deleteDate(event.dateId)
+            is TimelineDetailContract.TimelineDetailEvent.DeleteDate -> setState { copy(deleteLoadState = event.deleteLoadState) }
             is TimelineDetailContract.TimelineDetailEvent.SetLoadState -> setState { copy(loadState = event.loadState) }
             is TimelineDetailContract.TimelineDetailEvent.SetSideEffect -> setSideEffect(event.sideEffect)
         }
     }
 
-    fun fetchTimelineDetail(dateId: Int) {
-        setEvent(TimelineDetailContract.TimelineDetailEvent.FetchTimelineDetail(dateId))
-    }
-
-    fun setSourceScreen(source: Boolean) {
-        setEvent(TimelineDetailContract.TimelineDetailEvent.SetSourceScreen(sourceScreen = source))
-    }
-
-    fun onDeleteConfirm(dateId: Int) {
-        setEvent(TimelineDetailContract.TimelineDetailEvent.DeleteDate(dateId))
-    }
-
-    private fun fetchDateDetail(dateId: Int) {
+    fun fetchDateDetail(dateId: Int) {
         viewModelScope.launch {
-            setEvent(TimelineDetailContract.TimelineDetailEvent.SetLoadState(LoadState.Loading))
+            setEvent(TimelineDetailContract.TimelineDetailEvent.SetTimelineDetail(loadState = LoadState.Loading, dateDetail = currentState.dateDetail))
             getDateDetailUseCase(dateId).onSuccess { dateDetail ->
-                setEvent(TimelineDetailContract.TimelineDetailEvent.SetTimelineDetail(LoadState.Success, dateDetail))
+                setEvent(TimelineDetailContract.TimelineDetailEvent.SetTimelineDetail(loadState = LoadState.Success, dateDetail = dateDetail))
             }.onFailure {
-                setEvent(TimelineDetailContract.TimelineDetailEvent.SetTimelineDetail(LoadState.Error))
+                setEvent(TimelineDetailContract.TimelineDetailEvent.SetTimelineDetail(loadState = LoadState.Error, dateDetail = currentState.dateDetail))
             }
         }
     }
 
-    private fun deleteDate(dateId: Int) {
+    fun deleteDate(dateId: Int) {
         viewModelScope.launch {
-            setEvent(TimelineDetailContract.TimelineDetailEvent.SetLoadState(LoadState.Loading))
+            setEvent(TimelineDetailContract.TimelineDetailEvent.DeleteDate(deleteLoadState = LoadState.Loading))
             deleteDateUseCase(dateId).onSuccess {
-                setEvent(TimelineDetailContract.TimelineDetailEvent.SetTimelineDetail(LoadState.Success))
-                setEvent(TimelineDetailContract.TimelineDetailEvent.SetSideEffect(TimelineDetailContract.TimelineDetailSideEffect.PopBackStack))
+                setEvent(TimelineDetailContract.TimelineDetailEvent.DeleteDate(deleteLoadState = LoadState.Success))
             }.onFailure {
-                setEvent(TimelineDetailContract.TimelineDetailEvent.SetTimelineDetail(LoadState.Error))
+                setEvent(TimelineDetailContract.TimelineDetailEvent.DeleteDate(deleteLoadState = LoadState.Error))
             }
         }
-    }
-
-    private fun setTimelineDetail(loadState: LoadState, dateDetail: DateDetail?) {
-        setEvent(TimelineDetailContract.TimelineDetailEvent.SetTimelineDetail(loadState, dateDetail))
     }
 }
