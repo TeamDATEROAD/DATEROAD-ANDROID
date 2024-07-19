a6748dd6239ebcbb1bd2f7d9bde988908e63cafd
t1nm1ksun <minsuk07672@naver.com>
2024-07-19 22:42:15 +0900

    [chore] #162 프로필이미지 원으로 crop

--- app/src/main/java/org/sopt/dateroad/presentation/ui/profile/ProfileScreen.kt
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/profile/ProfileScreen.kt
@@ -15,12 +15,16 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
@@ -158,7 +162,10 @@ fun ProfileScreen(
                 modifier = Modifier
                     .height(128.dp)
                     .aspectRatio(1f)
+                    .clip(CircleShape),
+                contentScale = ContentScale.Crop
             )
+
 
             Image(
                 painter = painterResource(id = R.drawable.btn_my_profile_plus),
