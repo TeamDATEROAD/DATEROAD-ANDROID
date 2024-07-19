d9135155d9be8d1b11662d48ed087a6be3b99b8f
jihyunniiii <jita1125@naver.com>
2024-07-19 23:13:55 +0900

    [chore] #150 더블클릭 방지

--- app/src/main/java/org/sopt/dateroad/presentation/util/modifier/ModifierExt.kt
+++ app/src/main/java/org/sopt/dateroad/presentation/util/modifier/ModifierExt.kt
@@ -1,18 +1,33 @@
 package org.sopt.dateroad.presentation.util.modifier
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.input.pointer.pointerInteropFilter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalComposeUiApi::class)
 inline fun Modifier.noRippleClickable(
     crossinline onClick: () -> Unit = {}
 ): Modifier = composed {
-    this.clickable(
-        indication = null,
-        interactionSource = remember { MutableInteractionSource() }
-    ) {
-        onClick()
+    var clickable by remember { mutableStateOf(true) }
+
+    pointerInteropFilter {
+        if (clickable) {
+            onClick()
+            clickable = false
+            CoroutineScope(Dispatchers.Main).launch {
+                delay(500)
+                clickable = true
+            }
+        }
+        true
     }
 }
