045f0529f494cc7262eb1528451a43e523a51827
2hyunjinn <huenjin04@naver.com>
2024-08-06 16:14:23 +0900

    [feat] #175 토큰 재발급 api 연동

--- app/src/main/java/org/sopt/dateroad/data/dataremote/interceptor/AuthInterceptor.kt
+++ app/src/main/java/org/sopt/dateroad/data/dataremote/interceptor/AuthInterceptor.kt
@@ -1,29 +1,52 @@
 package org.sopt.dateroad.data.dataremote.interceptor
 
 import javax.inject.Inject
+import javax.inject.Provider
+import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
+import org.sopt.dateroad.data.dataremote.service.AuthService
 import org.sopt.dateroad.domain.usecase.GetAccessTokenUseCase
+import org.sopt.dateroad.domain.usecase.GetRefreshTokenUseCase
+import org.sopt.dateroad.domain.usecase.RefreshTokenUseCase
+import org.sopt.dateroad.domain.usecase.SaveAccessTokenUseCase
 
 class AuthInterceptor @Inject constructor(
-    private val getAccessTokenUseCase: GetAccessTokenUseCase
+    private val getAccessTokenUseCase: GetAccessTokenUseCase,
+    private val getRefreshTokenUseCase: GetRefreshTokenUseCase,
+    private val refreshTokenUseCase: RefreshTokenUseCase,
+    private val saveAccessTokenUseCase: SaveAccessTokenUseCase,
+    private val authServiceProvider: Provider<AuthService>
 ) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest = chain.request()
         val authRequest = originalRequest.newAuthBuilder()
 
-        val response = chain.proceed(authRequest)
+        var response = chain.proceed(authRequest)
 
-        when (response.code) {
-            CODE_TOKEN_EXPIRE -> {
-                // TODO 토큰 재발급 api 연동
+        if (response.code == CODE_TOKEN_EXPIRE) {
+            synchronized(this) {
+                response.close()
+                val refreshToken = runBlocking { getRefreshTokenUseCase() }
+                val newAuth = runBlocking { refreshTokenUseCase(refreshToken) }
+                runBlocking { saveAccessTokenUseCase(newAuth.accessToken) }
+
+                val newRequest = originalRequest.newBuilder()
+                    .removeHeader(AUTHORIZATION)
+                    .addHeader(AUTHORIZATION, BEARER + newAuth.accessToken)
+                    .build()
+
+                response = chain.proceed(newRequest)
             }
         }
+
         return response
     }
 
-    private fun Request.newAuthBuilder() = this.newBuilder().addHeader(AUTHORIZATION, getAccessTokenUseCase()).build()
+    private fun Request.newAuthBuilder() = this.newBuilder()
+        .addHeader(AUTHORIZATION, BEARER + runBlocking { getAccessTokenUseCase() })
+        .build()
 
     companion object {
         const val CODE_TOKEN_EXPIRE = 401
