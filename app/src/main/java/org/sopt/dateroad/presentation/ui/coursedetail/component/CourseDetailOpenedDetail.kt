c4f43059afeaeb68ffebc5eec807491ead692bd7
jihyunniiii <jita1125@naver.com>
2024-08-18 03:18:03 +0900

    [feat] #176 courseDetailOpenedDetail

---
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/coursedetail/component/CourseDetailOpenedDetail.kt
@@ -0,0 +1,37 @@
+package org.sopt.dateroad.presentation.ui.coursedetail.component
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import org.sopt.dateroad.domain.model.Place
+import org.sopt.dateroad.presentation.type.DateTagType
+import org.sopt.dateroad.ui.theme.DateRoadTheme
+
+fun LazyListScope.courseDetailOpenedDetail(
+    description: String,
+    places: List<Place>,
+    totalCost: String,
+    tags: List<DateTagType>
+) {
+    item {
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = description,
+            style = DateRoadTheme.typography.bodyMed13Context,
+            color = DateRoadTheme.colors.black
+        )
+    }
+    item {
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+    courseDetailTimeline(places = places)
+    item {
+        CourseDetailCost(totalCost = totalCost)
+    }
+    item {
+        CourseDetailTag(tags = tags)
+    }
+}
