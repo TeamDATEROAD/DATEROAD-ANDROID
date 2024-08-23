09ca94ca8795cd25f366001bb5988fbbbe8b1d5f
t1nm1ksun <minsuk07672@naver.com>
2024-08-23 15:54:11 +0900

    [chore] #186 에러 뷰 뒤로 가기 버튼 추가

--- app/src/main/java/org/sopt/dateroad/presentation/ui/component/view/DateRoadErrorView.kt
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/component/view/DateRoadErrorView.kt
@@ -1,5 +1,6 @@
 package org.sopt.dateroad.presentation.ui.component.view
 
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
@@ -18,19 +19,28 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.sopt.dateroad.R
+import org.sopt.dateroad.presentation.ui.component.topbar.DateRoadBasicTopBar
 import org.sopt.dateroad.ui.theme.DateRoadTheme
 
 @Composable
 fun DateRoadErrorView(
-    modifier: Modifier = Modifier
 ) {
+    val backDispatcher = LocalOnBackPressedDispatcherOwner.current?.onBackPressedDispatcher
     Column(
-        modifier = modifier
+        modifier = Modifier
             .fillMaxSize()
             .background(color = DateRoadTheme.colors.white),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Spacer(modifier = Modifier.weight(193f))
+        DateRoadBasicTopBar(
+            title = "",
+            iconLeftResource = R.drawable.ic_top_bar_back_white,
+            backGroundColor = DateRoadTheme.colors.white,
+            onIconClick =  {
+                backDispatcher?.onBackPressed()
+            }
+        )
+        Spacer(modifier = Modifier.weight(131f))
         Image(
             modifier = Modifier
                 .fillMaxWidth()
@@ -43,9 +53,18 @@ fun DateRoadErrorView(
         Text(
             modifier = Modifier
                 .fillMaxWidth(),
-            text = stringResource(id = R.string.error_view_server),
+            text = stringResource(id = R.string.error_view_server_title),
             color = DateRoadTheme.colors.gray500,
-            style = DateRoadTheme.typography.titleBold18,
+            style = DateRoadTheme.typography.titleExtra20,
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            modifier = Modifier
+                .fillMaxWidth(),
+            text = stringResource(id = R.string.error_view_server_description),
+            color = DateRoadTheme.colors.gray500,
+            style = DateRoadTheme.typography.bodyMed15,
             textAlign = TextAlign.Center
         )
         Spacer(modifier = Modifier.weight(186f))
