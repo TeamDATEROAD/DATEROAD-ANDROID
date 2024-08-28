67e2a4e8b93f6d33b20f1d7d76df9f066cf02e2d
jihyunniiii <jita1125@naver.com>
2024-08-28 16:04:51 +0900

    [chore] #205 포인트 내역 앞 +, - 추가

--- app/src/main/java/org/sopt/dateroad/data/mapper/todomain/ResponsePointHistoryDtoMapper.kt
+++ app/src/main/java/org/sopt/dateroad/data/mapper/todomain/ResponsePointHistoryDtoMapper.kt
@@ -4,6 +4,6 @@ import org.sopt.dateroad.data.dataremote.model.response.ResponsePointHistoryDto
 import org.sopt.dateroad.domain.model.PointHistory
 
 fun ResponsePointHistoryDto.toDomain(): PointHistory = PointHistory(
-    gained = this.gained.toDomain(),
-    used = this.used.toDomain()
+    gained = this.gained.toGainedPointDomain(),
+    used = this.used.toUsedPointDomain()
 )
--- app/src/main/java/org/sopt/dateroad/presentation/ui/pointhistory/component/PointHistoryCard.kt
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/pointhistory/component/PointHistoryCard.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
