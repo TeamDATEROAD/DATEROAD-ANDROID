2fe05b289ee50b62fa4a9ea63dc94e95a82bcb8e
jihyunniiii <jita1125@naver.com>
2024-09-10 17:41:36 +0900

    [chore] #247 MyCourse 엠티 뷰 정렬 제거

--- app/src/main/java/org/sopt/dateroad/presentation/ui/mycourse/MyCourseScreen.kt
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/mycourse/MyCourseScreen.kt
@@ -1,7 +1,6 @@
 package org.sopt.dateroad.presentation.ui.mycourse
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,13 +10,12 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.flowWithLifecycle
 import org.sopt.dateroad.R
@@ -111,18 +109,12 @@ fun MyCourseScreen(
         LazyColumn {
             if (myCourseUiState.courses.isEmpty()) {
                 item {
-                    Box(
-                        modifier = Modifier
-                            .fillParentMaxSize(),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        DateRoadEmptyView(
-                            emptyViewType = when (myCourseUiState.myCourseType) {
-                                MyCourseType.ENROLL -> EmptyViewType.MY_COURSE_ENROLL
-                                MyCourseType.READ -> EmptyViewType.MY_COURSE_READ
-                            }
-                        )
-                    }
+                    DateRoadEmptyView(
+                        emptyViewType = when (myCourseUiState.myCourseType) {
+                            MyCourseType.ENROLL -> EmptyViewType.MY_COURSE_ENROLL
+                            MyCourseType.READ -> EmptyViewType.MY_COURSE_READ
+                        }
+                    )
                 }
             }
             items(myCourseUiState.courses) { course ->
