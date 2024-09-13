ca4516f502196e4dc58757753a536de148c3ca08
t1nm1ksun <minsuk07672@naver.com>
2024-09-13 16:19:38 +0900

    [chore] #249 하단 버튼 사이즈 기기대응

--- app/src/main/java/org/sopt/dateroad/presentation/ui/coursedetail/component/CourseDetailBottomBar.kt
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/coursedetail/component/CourseDetailBottomBar.kt
@@ -5,11 +5,18 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,6 +32,8 @@ fun CourseDetailBottomBar(
     onLikeButtonClicked: () -> Unit,
     onEnrollButtonClicked: () -> Unit
 ) {
+    var buttonHeight by remember { mutableStateOf(0.dp) } // 버튼 크기를 저장하는 상태 변수
+    val density = LocalDensity.current
     Row(
         modifier = modifier
             .fillMaxWidth()
@@ -32,6 +41,7 @@ fun CourseDetailBottomBar(
             .padding(horizontal = 16.dp, vertical = 16.dp)
     ) {
         DateRoadImageButton(
+            modifier = Modifier.height(buttonHeight),
             iconResId = R.drawable.ic_coures_detail_heart_default,
             enabledContentColor = DateRoadTheme.colors.purple600,
             disabledContentColor = DateRoadTheme.colors.gray200,
@@ -45,7 +55,11 @@ fun CourseDetailBottomBar(
         )
         Spacer(modifier = Modifier.width(12.dp))
         DateRoadBasicButton(
-            modifier = Modifier.weight(1f),
+            modifier = Modifier
+                .weight(1f)
+                .onSizeChanged { size ->
+                    buttonHeight = with(density) { size.height.toDp() }
+                },
             textContent = stringResource(id = R.string.course_detail_get_course),
             onClick = onEnrollButtonClicked
         )
