d8f6bf53464ed206f0b1917a4d2b6df412f98011
t1nm1ksun <minsuk07672@naver.com>
2024-09-13 16:40:19 +0900

    [chore] #251 ktlintFormat

--- app/src/main/java/org/sopt/dateroad/presentation/util/Constraints.kt
+++ app/src/main/java/org/sopt/dateroad/presentation/util/Constraints.kt
@@ -49,7 +49,7 @@ object Token {
 object Time {
     const val TIME = " 시간"
 }
-object LoadingView{
+object LoadingView {
     const val LOTTIE = "loading.json"
     const val CLIPMIN = 0
     const val CLIPMAX = 1200
