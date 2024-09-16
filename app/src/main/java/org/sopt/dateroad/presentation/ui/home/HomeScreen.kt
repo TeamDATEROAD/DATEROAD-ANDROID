fd1f3dfea5293247bfc36d0a33e2bdc27f9bf08d
t1nm1ksun <minsuk07672@naver.com>
2024-09-16 21:46:30 +0900

    [chore] #234 코스 프로퍼티 new 코스 추가

--- app/src/main/java/org/sopt/dateroad/presentation/ui/home/HomeScreen.kt
+++ app/src/main/java/org/sopt/dateroad/presentation/ui/home/HomeScreen.kt
@@ -122,10 +122,10 @@ fun HomeRoute(
                 mapOf(
                     USER_NAME to uiState.userPoint.name,
                     USER_POINT to uiState.userPoint.point,
-                    COURSE_LIST_ID to uiState.topLikedCourses.map { it.courseId }.joinToString(),
-                    COURSE_LIST_TITLE to uiState.topLikedCourses.joinToString { it.title },
-                    COURSE_LIST_LOCATION to uiState.topLikedCourses.joinToString { it.city },
-                    COURSE_LIST_COST to uiState.topLikedCourses.joinToString { it.cost }
+                    COURSE_LIST_ID to uiState.topLikedCourses.map { it.courseId }.joinToString() + "," + uiState.latestCourses.map { it.courseId }.joinToString(),
+                    COURSE_LIST_TITLE to uiState.topLikedCourses.joinToString { it.title } + "," + uiState.latestCourses.joinToString { it.title },
+                    COURSE_LIST_LOCATION to uiState.topLikedCourses.joinToString { it.city } + "," + uiState.latestCourses.joinToString { it.city },
+                    COURSE_LIST_COST to uiState.topLikedCourses.joinToString { it.cost } + "," + uiState.latestCourses.joinToString { it.cost }
                 )
             )
         }
